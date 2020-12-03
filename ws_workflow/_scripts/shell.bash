f418a0832f6bd87b8206823ed744874d08cf1b38
Shane Loretz <sloretz@openrobotics.org>
2020-12-03 12:16:35 -0800

    Use realpath so --pwd argument works (#4)
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- ws_workflow/_scripts/shell.bash
+++ ws_workflow/_scripts/shell.bash
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Change directory because it doesn't exist in container, and so won't be bound
 cd $DIR
-WS_DIR=$DIR/..
+WS_DIR=$(realpath $DIR/..)
 
 . __find_sandbox.bash
 
